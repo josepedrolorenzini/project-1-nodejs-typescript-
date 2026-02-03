439c3b6f6c127be16093fa030b331d94262ceaf2
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2026-02-03 16:47:54 +1100

    React Project 1

---
+++ backend/src/data/mathF.ts
@@ -0,0 +1,5 @@
+const whatsMyAge = (): number => {
+  return Math.floor(Math.random()*120 + 1);
+};
+
+console.log(`I am ${whatsMyAge()} years old today!`);
--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -5,7 +5,7 @@ export const pool = new Pool({
         host: 'localhost',
         port: 5432,
         user: 'postgres',
-        password: 'postgres',
+        password: 'xxx',
         database: 'tsnode',
 }) ; 
 
